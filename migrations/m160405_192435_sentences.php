e447e828ff2d4e1ce0b5c99434550edb0501426b
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2016-04-05 23:07:19 +0300

    update words migration and sentences migration

--- migrations/m160405_192435_sentences.php
+++ migrations/m160405_192435_sentences.php
@@ -3,21 +3,22 @@
 use yii\db\Migration;
 use yii\db\Schema;
 
-class m160301_135956_words extends Migration
+class m160405_192435_sentences extends Migration
 {
     public function up()
     {
-        $this->createTable('words', [
+        $this->createTable('sentences', [
             'id'        => Schema::TYPE_PK,
-            'word'      => Schema::TYPE_STRING  . ' NOT NULL',
+            'phrase'    => Schema::TYPE_STRING  . ' NOT NULL',
             'translate' => Schema::TYPE_STRING  . ' NOT NULL',
             'month'     => Schema::TYPE_STRING  . ' NOT NULL',
             'year'      => Schema::TYPE_INTEGER . ' NOT NULL',
+            'category'  => Schema::TYPE_STRING  . ' NOT NULL',
         ], 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
     }
 
     public function down()
     {
-        $this->dropTable('words');
+        $this->dropTable('sentences');
     }
 }
