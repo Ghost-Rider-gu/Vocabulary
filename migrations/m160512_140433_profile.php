ecc0703cfebd33b743811ee827cf0478243d678a
Golubnichenko Yura <ghostrider.gu@gmail.com>
2016-05-12 17:32:18 +0300

    add profile migration

---
+++ migrations/m160512_140433_profile.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m160512_140433_profile extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160512_140433_profile cannot be reverted.\n";
+
+        return false;
+    }
+}
