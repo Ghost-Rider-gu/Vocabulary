2b6aefe81357beb211745e2e159c23a1c14eef78
Golubnichenko Yura <ghostrider.gu@gmail.com>
2016-05-12 17:32:00 +0300

    update SiteController

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -7,7 +7,6 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
-use app\models\ContactForm;
 
 class SiteController extends Controller
 {
