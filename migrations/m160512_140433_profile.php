1b1d1530a30c9d47d0b2b83761a57077203454db
Golubnichenko Yura <ghostrider.gu@gmail.com>
2016-05-18 17:15:46 +0300

    update migration

--- migrations/m160512_140433_profile.php
+++ migrations/m160512_140433_profile.php
@@ -7,7 +7,13 @@ use yii\db\Schema;
  * This class of migration for 'profile' table
  *
  * Fields:
- *
+ *      id          - unique identification word
+ *      user_id     - user profile
+ *      nickname    - user login (register)
+ *      email       - user email (or empty)
+ *      website     - user website (or empty)
+ *      avatar      - user pict (or empty)
+ *      realname    - user real name (or empty)
  */
 class m160512_140433_profile extends Migration
 {
@@ -19,6 +25,11 @@ class m160512_140433_profile extends Migration
         $this->createTable('profile', [
             'id'        => Schema::TYPE_PK,
             'user_id'   => Schema::TYPE_INTEGER . ' NOT NULL',
+            'nickname'  => Schema::TYPE_STRING  . ' NOT NULL',
+            'email'     => Schema::TYPE_STRING  . ' NULL',
+            'website'   => Schema::TYPE_STRING  . ' NULL',
+            'avatar'    => Schema::TYPE_STRING  . ' NULL',
+            'realname'  => Schema::TYPE_STRING  . ' NULL',
         ], 'ENGINE=InnoDB DEFAULT CHARSET=utf8');
     }
 
