055470807c7e4e8639ed2996bb223f89f915f91d
Golubnichenko aka Ghost Rider <ghostrider.gu@gmail.com>
2017-07-01 22:25:34 +0300

    add DB (sql script)

---
+++ app.resources/create_db.sql
@@ -0,0 +1,109 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema engbang
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `engbang` DEFAULT CHARACTER SET utf8 ;
+USE `engbang` ;
+
+-- -----------------------------------------------------
+-- Table `engbang`.`user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `engbang`.`user` ;
+
+CREATE TABLE IF NOT EXISTS `engbang`.`user` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `login` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(15) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `engbang`.`profile`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `engbang`.`profile` ;
+
+CREATE TABLE IF NOT EXISTS `engbang`.`profile` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `user_mail` VARCHAR(30) NULL,
+  `user_avatar` VARCHAR(45) NULL,
+  `user_name` VARCHAR(80) NULL,
+  `user_describe` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`, `user_id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
+  CONSTRAINT `profile_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `engbang`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `engbang`.`word_category`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `engbang`.`word_category` ;
+
+CREATE TABLE IF NOT EXISTS `engbang`.`word_category` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `category_UNIQUE` (`category` ASC))
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `engbang`.`word`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `engbang`.`word` ;
+
+CREATE TABLE IF NOT EXISTS `engbang`.`word` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `word` VARCHAR(50) NOT NULL,
+  `translate` VARCHAR(60) NOT NULL,
+  `category_id` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`, `category_id`),
+  INDEX `category_id_idx` (`category_id` ASC),
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `engbang`.`word_category` (`category`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+-- -----------------------------------------------------
+-- Table `engbang`.`word_bind`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `engbang`.`word_bind` ;
+
+CREATE TABLE IF NOT EXISTS `engbang`.`word_bind` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `word_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `month` VARCHAR(15) NOT NULL,
+  `year` INT NOT NULL,
+  `full_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`, `user_id`, `word_id`),
+  INDEX `word_id_idx` (`word_id` ASC),
+  INDEX `user_id_idx` (`user_id` ASC),
+  CONSTRAINT `word_id`
+    FOREIGN KEY (`word_id`)
+    REFERENCES `engbang`.`word` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `engbang`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
