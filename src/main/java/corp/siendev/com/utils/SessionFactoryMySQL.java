b62b0d43c8696c955683600d7bacf9b2d935bd72
Golubnichenko aka Ghost Rider <ghostrider.gu@gmail.com>
2017-07-08 22:55:46 +0300

    update SessionFactoryMySQL

--- src/main/java/corp/siendev/com/utils/SessionFactoryMySQL.java
+++ src/main/java/corp/siendev/com/utils/SessionFactoryMySQL.java
@@ -19,6 +19,8 @@ public class SessionFactoryMySQL
 
     public static void closeSessionFactoryMySQL()
     {
-        sessionFactoryMySql.close();
+        if(sessionFactoryMySql.isOpen()) {
+            sessionFactoryMySql.close();
+        }
     }
 }
