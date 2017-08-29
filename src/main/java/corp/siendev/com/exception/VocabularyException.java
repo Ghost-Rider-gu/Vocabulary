178f3cd3ba990155cce19df8e32875f9747537f7
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-08-29 21:21:35 +0300

    update structure of project

--- src/main/java/corp/siendev/com/exception/VocabularyException.java
+++ src/main/java/corp/siendev/com/exception/VocabularyException.java
@@ -1,11 +1,18 @@
 package corp.siendev.com.exception;
 
 /**
- * General exception for application
+ * General exception for application.
  *
  * @author Golubnichenko Yuriy
  */
-public class VocabularyException extends Exception
-{
+public class VocabularyException extends Exception {
+
+    public VocabularyException(String message) {
+        super(message);
+    }
+
+    public VocabularyException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
