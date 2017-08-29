07036100f309c02d2f2ffc63643a081b8565f030
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-08-29 21:21:18 +0300

    add several new files

---
+++ src/main/java/corp/siendev/com/VocabularyApplication.java
@@ -0,0 +1,18 @@
+package corp.siendev.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Vocabulary application invoker.
+ *
+ * @author Golubnichenko Yuriy
+ */
+@SpringBootApplication
+public class VocabularyApplication {
+
+    public static void main(String args[]) {
+        SpringApplication.run(VocabularyApplication.class, args);
+    }
+
+}
