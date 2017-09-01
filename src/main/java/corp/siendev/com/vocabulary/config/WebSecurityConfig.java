fba7160cf909a1421217c86c27fe2a0e3689881e
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-09-01 23:37:42 +0300

    add config app

---
+++ src/main/java/corp/siendev/com/vocabulary/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package corp.siendev.com.vocabulary.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalAuthentication
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").authenticated()
+                .and().formLogin().loginPage("/login").permitAll()
+                .and().logout().permitAll();
+    }
+
+}
