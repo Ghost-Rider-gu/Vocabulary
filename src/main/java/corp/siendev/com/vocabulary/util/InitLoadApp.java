61af6f00a6fce40d023205cfe9ba40c5a39c4efc
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-09-01 23:38:47 +0300

    add preload data

---
+++ src/main/java/corp/siendev/com/vocabulary/util/InitLoadApp.java
@@ -0,0 +1,21 @@
+package corp.siendev.com.vocabulary.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Initial before load application.
+ *
+ * @author Golubnichenko Yuriy
+ */
+@Slf4j
+@Component
+public class InitLoadApp implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+
+}
