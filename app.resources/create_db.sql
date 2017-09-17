938e8074d5997fb2bfd521ff4be938ed499a6991
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-09-17 21:58:10 +0300

    update schema

--- app.resources/create_db.sql
+++ app.resources/create_db.sql
@@ -3,92 +3,83 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema engbang
+-- Schema vocabulary
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `engbang` ;
+DROP SCHEMA IF EXISTS `vocabulary` ;
 
 -- -----------------------------------------------------
--- Schema engbang
+-- Schema vocabulary
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `engbang` DEFAULT CHARACTER SET utf8 ;
-USE `engbang` ;
+CREATE SCHEMA IF NOT EXISTS `vocabulary` DEFAULT CHARACTER SET utf8 ;
+USE `vocabulary` ;
 
 -- -----------------------------------------------------
--- Table `engbang`.`profile`
+-- Table `vocabulary`.`word_category`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `engbang`.`profile` ;
-
-CREATE TABLE IF NOT EXISTS `engbang`.`profile` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user_mail` VARCHAR(30) NULL,
-  `user_avatar` VARCHAR(45) NULL,
-  `user_name` VARCHAR(80) NULL,
-  `user_describe` VARCHAR(100) NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB;
-
--- -----------------------------------------------------
--- Table `engbang`.`word_category`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `engbang`.`word_category` ;
-
-CREATE TABLE IF NOT EXISTS `engbang`.`word_category` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `category` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `vocabulary`.`word_category` (
+  `id` INT NOT NULL,
+  `category` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `category_UNIQUE` (`category` ASC))
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `engbang`.`word`
+-- Table `vocabulary`.`user`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `engbang`.`word` ;
+CREATE TABLE IF NOT EXISTS `vocabulary`.`user` (
+  `id` INT NOT NULL,
+  `login` VARCHAR(40) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `login_UNIQUE` (`login` ASC))
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS `engbang`.`word` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `category_id` INT NOT NULL,
-  `word` VARCHAR(50) NOT NULL,
-  `translate` VARCHAR(60) NOT NULL,
-  `month` VARCHAR(15) NOT NULL,
-  `year` INT NOT NULL,
-  `full_date` DATETIME NOT NULL,
-  PRIMARY KEY (`id`, `category_id`),
-  INDEX `category_id_idx` (`category_id` ASC),
-  CONSTRAINT `category_id`
-    FOREIGN KEY (`category_id`)
-    REFERENCES `engbang`.`word_category` (`id`)
-    ON DELETE CASCADE
+-- -----------------------------------------------------
+-- Table `vocabulary`.`profile`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `vocabulary`.`profile` (
+  `id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  `user_mail` VARCHAR(30) NULL,
+  `user_avatar` VARCHAR(50) NULL,
+  `user_name` VARCHAR(40) NULL,
+  `user_describe` VARCHAR(100) NULL,
+  PRIMARY KEY (`id`, `user_id`),
+  INDEX `user_id_idx` (`user_id` ASC),
+  CONSTRAINT `user_id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `vocabulary`.`user` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `engbang`.`user`
+-- Table `vocabulary`.`word`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `engbang`.`user` ;
-
-CREATE TABLE IF NOT EXISTS `engbang`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `profile_id` INT NOT NULL,
-  `word_id` INT NOT NULL,
-  `login` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(15) NOT NULL,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NOT NULL,
-  `deleted_at` DATETIME NULL,
-  PRIMARY KEY (`id`, `profile_id`, `word_id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  UNIQUE INDEX `profile_id_UNIQUE` (`profile_id` ASC),
-  UNIQUE INDEX `word_id_UNIQUE` (`word_id` ASC),
-  CONSTRAINT `profile_id`
-    FOREIGN KEY (`profile_id`)
-    REFERENCES `engbang`.`profile` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT `wor_id`
-    FOREIGN KEY (`word_id`)
-    REFERENCES `engbang`.`word` (`id`)
-    ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS `vocabulary`.`word` (
+  `id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  `id_user` INT NOT NULL,
+  `word` VARCHAR(60) NOT NULL,
+  `translate` VARCHAR(100) NOT NULL,
+  `month` VARCHAR(20) NULL,
+  `year` INT NULL,
+  `full_date` DATETIME NOT NULL,
+  PRIMARY KEY (`id`, `id_user`, `category_id`),
+  INDEX `category_id_idx` (`category_id` ASC),
+  INDEX `user_id_idx` (`id_user` ASC),
+  CONSTRAINT `category_id`
+    FOREIGN KEY (`category_id`)
+    REFERENCES `vocabulary`.`word_category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_user`
+    FOREIGN KEY (`id_user`)
+    REFERENCES `vocabulary`.`user` (`id`)
+    ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
