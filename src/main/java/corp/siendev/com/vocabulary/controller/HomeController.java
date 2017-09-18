f07bd9b7f90832c6c294ce243ab9d7b3e6dd4873
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-09-18 23:12:16 +0300

    add HomeController

---
+++ src/main/java/corp/siendev/com/vocabulary/controller/HomeController.java
@@ -0,0 +1,16 @@
+package corp.siendev.com.vocabulary.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Main page of Vocabulary.
+ *
+ * @author Golubnichenko Yuriy
+ */
+@Controller
+@RequestMapping(value = "vocabulary")
+public class HomeController {
+
+
+}
