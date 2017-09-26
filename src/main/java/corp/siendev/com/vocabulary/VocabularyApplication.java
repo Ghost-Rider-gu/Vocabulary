6a6c32db67ed3cfef147f98a5fc56114042d6dba
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-09-26 23:33:00 +0300

    update main class

--- src/main/java/corp/siendev/com/vocabulary/VocabularyApplication.java
+++ src/main/java/corp/siendev/com/vocabulary/VocabularyApplication.java
@@ -3,7 +3,10 @@ package corp.siendev.com.vocabulary;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.Date;
 
 /**
  * Vocabulary application invoker.
@@ -11,12 +14,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @author Golubnichenko Yuriy
  */
 @Slf4j
+@EntityScan("corp.siendev.com.vocabulary.model")
+@EnableJpaRepositories("corp.siendev.com.vocabulary.repository")
 @SpringBootApplication
-@EnableScheduling
 public class VocabularyApplication {
 
     public static void main(String args[]) {
-        log.info("Start application");
+        log.info("Start application: " + new Date());
         SpringApplication.run(VocabularyApplication.class, args);
     }
 
