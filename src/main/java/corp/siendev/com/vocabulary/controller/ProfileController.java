452b5cd0059f8adca328243c2454bfd8294dd0ee
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2017-10-25 23:45:40 +0300

    add profile controller template

---
+++ src/main/java/corp/siendev/com/vocabulary/controller/ProfileController.java
@@ -0,0 +1,13 @@
+package corp.siendev.com.vocabulary.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Controller for user personal page.
+ *
+ * @author Golubnichenko Yuriy
+ */
+@Controller
+public class ProfileController {
+
+}
