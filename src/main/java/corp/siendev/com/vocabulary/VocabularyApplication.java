f9e3f80e367009d96ef09db80723ea34d112bef3
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2018-08-20 00:01:20 +0300

    update

--- src/main/java/corp/siendev/com/vocabulary/VocabularyApplication.java
+++ src/main/java/corp/siendev/com/vocabulary/VocabularyApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Date;
 
@@ -16,6 +17,7 @@ import java.util.Date;
 @Slf4j
 @EntityScan("corp.siendev.com.vocabulary.model")
 @EnableJpaRepositories("corp.siendev.com.vocabulary.repository")
+@EnableScheduling
 @SpringBootApplication
 public class VocabularyApplication {
 
--- src/main/java/corp/siendev/com/vocabulary/util/InitLoadApp.java
+++ src/main/java/corp/siendev/com/vocabulary/util/InitLoadApp.java
@@ -1,7 +1,10 @@
 package corp.siendev.com.vocabulary.util;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,9 +16,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class InitLoadApp implements CommandLineRunner {
 
+    @Autowired
+    private DataSource ds;
+
     @Override
     public void run(String... args) throws Exception {
         log.info("Initialization data ...");
+        log.info(ds.toString());
     }
 
+    @Scheduled(initialDelay = 10000, fixedRate = 10000)
+    public void run() {
+        log.info("test");
+    }
 }
