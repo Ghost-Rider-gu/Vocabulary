6a0f9514ea686323be7d295f0c7c2c0d7b70fea2
Iurii Golubnichenko <ghostrider.gu@gmail.com>
2021-11-13 22:29:39 +0300

    swagger updates

---
+++ src/main/java/corp/siendev/com/verbarium/config/SwaggerConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2021. SienDev, Inc. and Golubnichenko Iurii.  All Rights Reserved.
+ */
+
+package corp.siendev.com.verbarium.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Verbarium APIs",
+                "APIs for working with vocabulary app",
+                "1.0",
+                "Free and Open Source. It was built for T-Shape engineering course",
+                new Contact("Iurii Golubnichenko", "https://github.com/Ghost-Rider-gu", "ghostrider.gu@gmail.com"),
+                "GNU GENERAL PUBLIC LICENSE",
+                "http://www.gnu.org/licenses/",
+                Collections.emptyList());
+    }
+}
--- src/main/java/corp/siendev/com/verbarium/exception/RestExceptionHandler.java
+++ src/main/java/corp/siendev/com/verbarium/exception/RestExceptionHandler.java
@@ -6,16 +6,18 @@ package corp.siendev.com.verbarium.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<ResponseErrorDetails> handleResourceNotFoundException(ResourceNotFoundException resourceNotFoundEx,
                                                              HttpServletRequest httpServletRequest) {
         String stackTrace = getStackTrace(resourceNotFoundEx);
@@ -27,6 +29,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(ResourceAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ResponseEntity<ResponseErrorDetails> handleResourceAlreadyExistsException(ResourceAlreadyExistsException resourceAlreadyExistsEx,
                                                              HttpServletRequest httpServletRequest) {
         String stackTrace = getStackTrace(resourceAlreadyExistsEx);
@@ -38,6 +41,7 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ResponseErrorDetails> handleBadRequestException(BadRequestException badRequestEx,
                                                                           HttpServletRequest httpServletRequest) {
         String stackTrace = getStackTrace(badRequestEx);
